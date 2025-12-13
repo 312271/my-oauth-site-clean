08c720bdd4f29746bd6dc09c22cb65ad509f62e8
312271 <114382163+312271@users.noreply.github.com>
2025-12-14 01:19:14 +0300

    Update token.js

--- token.js
+++ token.js
@@ -1,13 +1,6 @@
+
 window.onload = () => {
-  console.log('Token page loaded');
-  
-  // Проверьте, что функция существует
-  if (typeof YaSendSuggestToken === 'function') {
-    console.log('Calling YaSendSuggestToken');
-    YaSendSuggestToken('https://my-oauth-site-clean.vercel.app', {
-      flag: true
-    });
-  } else {
-    console.error('YaSendSuggestToken not found');
-  }
+  YaSendSuggestToken('https://my-oauth-site-clean.vercel.app', {
+    flag: true
+  });
 };
