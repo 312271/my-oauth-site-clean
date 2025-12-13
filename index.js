ff448b0952e5ff7aee396c5f783b56137f5297ca
312271 <114382163+312271@users.noreply.github.com>
2025-12-14 01:03:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
       client_id: '2ac30da3005b46029619b9c3a7388b26',
       response_type: 'token',
       redirect_uri: 'https://my-oauth-site-clean.vercel.app/token.html'
-    }, 'https://my-oauth-site-clean.vercel.app')
+    }, 
+      'https://my-oauth-site-clean.vercel.app'
+  )
       .then(({ handler }) => handler())
       .then(async (data) => {
         const user = await fetch(`https://login.yandex.ru/info?format=json&oauth_token=${data.access_token}`).then(r => r.json());
