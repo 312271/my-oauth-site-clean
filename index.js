2ae113eabc7dbba63babe4148a0d4e810972dd93
312271 <114382163+312271@users.noreply.github.com>
2025-12-14 01:05:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
   if (!button) return;
 
   button.addEventListener('click', () => {
-    window.YaAuthSuggest.init({
-      client_id: '2ac30da3005b46029619b9c3a7388b26',
-      response_type: 'token',
-      redirect_uri: 'https://my-oauth-site-clean.vercel.app/token.html'
-    }, 
+        window.YaAuthSuggest.init(
+      '2ac30da3005b46029619b9c3a7388b26',
+      {
+        response_type: 'token',
+        redirect_uri: 'https://my-oauth-site-clean.vercel.app/token.html'
+      },
       'https://my-oauth-site-clean.vercel.app'
-  )
+    )
       .then(({ handler }) => handler())
       .then(async (data) => {
         const user = await fetch(`https://login.yandex.ru/info?format=json&oauth_token=${data.access_token}`).then(r => r.json());
