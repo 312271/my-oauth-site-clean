2001ec897163bd5804331b2e2d5b4c9a50c9cd76
312271 <114382163+312271@users.noreply.github.com>
2025-12-14 01:31:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const button = document.getElementById('authButton');
-  if (!button) return;
-
-  button.addEventListener('click', () => {
-      window.YaAuthSuggest.init({
-      client_id: '2ac30da3005b46029619b9c3a7388b26',
-      response_type: 'token',
-      redirect_uri: 'https://my-oauth-site-clean.vercel.app/token.html'
-    }, 'https://my-oauth-site-clean.vercel.app')
-      .then(({ handler }) => handler())
-      .then(async (data) => {
-        const user = await fetch(`https://login.yandex.ru/info?format=json&oauth_token=${data.access_token}`).then(r => r.json());
-        button.innerText = `Привет, ${user.first_name}!`;
-        button.disabled = true;
-      })
-      .catch(err => {
-        console.error('Ошибка:', err);
-        alert('Не удалось авторизоваться');
-      });
-  });
-});
+document.getElementById("suggest").onclick = () => {
+  YaAuthSuggest.init(
+    {
+      client_id: "2ac30da3005b46029619b9c3a7388b26",
+      response_type: "token",
+      redirect_url: "https://my-oauth-site-clean.vercel.app/token.html",
+    },
+    "https://my-oauth-site-clean.vercel.app"
+  )
+    .then(({ handler }) => handler())
+    .then(async (data) => {
+      const result = await fetchYandexData(data.access_token);
+      authorize(result);
+      console.log(result, data);
+    })
+    .catch((error) => console.log("Что-то пошло не так: ", error));
+};
